3ca29360b5f0b38b03c2d0eb1b812bb5cecf6a2f
Ariel221 <arielgalvez@unitec.edu>
2018-08-10 15:18:22 -0600

    ya termine esta clase

--- Lab4_DavidAriel/src/lab4_davidariel/Lab4_DavidAriel.java
+++ Lab4_DavidAriel/src/lab4_davidariel/Lab4_DavidAriel.java
@@ -5,12 +5,71 @@
  */
 package lab4_davidariel;
 
+import java.util.Scanner;
+
 /**
  *
  * @author david
  */
 public class Lab4_DavidAriel {
-    public static void main(String[] args){
-        System.out.println("pene");
+
+    static Scanner leer = new Scanner(System.in);
+    static int opcion;
+
+    public static void main(String[] args) {
+        char resp = 's';
+        do {
+            switch (menu()) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    //crud para los pilotos
+                    
+                    
+                    break;
+                case 5:
+                    System.out.println("Se termino todo");
+                    break;
+                default:
+                    System.out.println("Ingreso una opcion equivocada");
+            }//fin del switch
+            System.out.println("Se desea salir[S/N]:");
+            resp = leer.next().charAt(0);
+
+        } while (resp == 'S' || resp == 's');
+
     }
+
+    static void imprimir(Object[][] mat) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (mat[i][j] == (null)) {
+                    System.out.print("[" + " " + "]");
+                } else {
+                    System.out.print("[" + mat[i][j] + "]");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static int menu() {
+        System.out.println("Bienvenido!!!!!!!!!!!!!");
+        System.out.println("---------------------------");
+        System.out.println("1.Listas de Evas");
+        System.out.println("2.Lista de Angeles");
+        System.out.println("3.CRUD");
+        System.out.println("4.Simulacion");
+        System.out.println("5.Salir");
+        System.out.println("----------------------------");
+        System.out.print("Ingrese una opcion: ");
+        opcion = leer.nextInt();
+        return opcion;
+
+    }//Fin del menu
+
 }
